dcd1b4840df3c566c9d30bb986f2e5dca4d7913a
daneker <kkraken2005@gmail.com>
2023-02-24 23:28:40 +0600

    To Do Application

---
+++ src/main/java/daneker/code/todo_web_application/ToDoWebApplication.java
@@ -0,0 +1,13 @@
+package daneker.code.todo_web_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/daneker/code/todo_web_application/TodoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package daneker.code.todo_web_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
